c2a5ab4bb256eae7f8978857f56cfccb23dcbe6d
TheTechGuy-PH4 <PhillipHogan1@outlook.com>
2024-07-22 14:08:24 -0700

    Exercise 2

--- Labfiles/01-use-azure-ai-services/C-Sharp/rest-client/obj/Debug/net7.0/rest_client.AssemblyInfo.cs
+++ Labfiles/01-use-azure-ai-services/C-Sharp/rest-client/obj/Debug/net7.0/rest_client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("rest_client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9799cd58c60f9134b8ea6d1aae7059cf152fa048")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7164a423c7125e858970635b641df98cef5a650")]
 [assembly: System.Reflection.AssemblyProductAttribute("rest_client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("rest_client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
